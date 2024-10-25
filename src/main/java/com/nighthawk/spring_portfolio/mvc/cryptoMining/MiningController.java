c7efa00f4fed1ade5db1ec01cca9236944fe642a
JasonGuan1012 <jasonguan57@gmail.com>
2024-10-24 18:22:19 -0700

    request testing

--- src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/MiningController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/MiningController.java
@@ -1,30 +1,42 @@
 package com.nighthawk.spring_portfolio.mvc.cryptoMining;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/api/mining")
+@RequestMapping("/api")
 public class MiningController {
 
-    @Autowired
-    private MiningService miningService;
-
-    @GetMapping("/")
-    public List<String> getAllMiningData() {
-        return miningService.getMiningData();
-    }
-
+    // Handle mining request
     @PostMapping("/mine")
-    public String performMining() {
-        return miningService.mine();
+    public ResponseEntity<String> mineCrypto(@RequestBody MiningData data) {
+        // For now, we just print the received data from frontend
+        System.out.println("Mining request received: " + data.getMiningPower() + " coins.");
+        return new ResponseEntity<>("Mining successful!", HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public String addMiningData(@RequestParam String data) {
-        miningService.addMiningData(data);
-        return "Data added successfully!";
+    // Handle upgrade request
+    @PostMapping("/upgrade")
+    public ResponseEntity<String> upgradeMiningPower(@RequestBody MiningData data) {
+        if (data.getCoins() >= 10) {
+            System.out.println("Upgrade request received: New mining power: " + data.getMiningPower());
+            return new ResponseEntity<>("Upgrade successful!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Not enough coins!", HttpStatus.BAD_REQUEST);
+        }
     }
+}
+
+// Data class to hold mining data from frontend
+class MiningData {
+    private int coins;
+    private int miningPower;
+
+    // Getters and Setters
+    public int getCoins() { return coins; }
+    public void setCoins(int coins) { this.coins = coins; }
+    
+    public int getMiningPower() { return miningPower; }
+    public void setMiningPower(int miningPower) { this.miningPower = miningPower; }
 }
