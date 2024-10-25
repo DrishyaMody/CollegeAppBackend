1d375b02d05fcba720452d591a88a0325d5e2baa
JasonGuan1012 <jasonguan57@gmail.com>
2024-10-24 18:28:49 -0700

    still testing

--- src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/MiningController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/MiningController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/cryptoMining")
 public class MiningController {
 
     // Handle mining request
